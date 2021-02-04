9d7a1009b6697299df86b6c04409b80e82b01c8f
SMhero <smhero62@gmail.com>
2021-02-04 21:10:24 +0300

    Updates

---
+++ precache-manifest.83248c274bd79673566527a001d1714f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee9fcfed0bbc5297885e474396aceb0",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "6b74756f4e87fc879fca",
+    "url": "/calculator-react/static/css/main.37dfab79.chunk.css"
+  },
+  {
+    "revision": "637bd4136bb679045bcd",
+    "url": "/calculator-react/static/js/2.290ec7d6.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator-react/static/js/2.290ec7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b74756f4e87fc879fca",
+    "url": "/calculator-react/static/js/main.5c6bd103.chunk.js"
+  },
+  {
+    "revision": "928bf717d83facf55a8d",
+    "url": "/calculator-react/static/js/runtime-main.f105f787.js"
+  },
+  {
+    "revision": "4b9bd167e8a55c9cc9a4d072669e150d",
+    "url": "/calculator-react/static/media/logo.4b9bd167.svg"
+  }
+]);
